20c53aefd423eaffd3c75e79258cbbe443a2b88d
Macha Celeste <shetah1@yahoo.com>
2025-05-01 14:37:27 -0400

    Add project files.

---
+++ patches/ComputerIntrinsicsPatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using Miniscript;
+using NetworkMessages;
+using System.Collections.Generic;
+
+[HarmonyPatch]
+class ComputerIntrinsicsPatch
+{
+    private static bool intrinsicsAdded;
+
+    [HarmonyPatch(typeof(ComputerIntrinsics), "AddInstrinsics")]
+    static void Postfix()
+    {
+        if (intrinsicsAdded == true)
+            return;
+        intrinsicsAdded = true;
+        Intrinsic intrinsic = Intrinsic.Create("disconnect_network");
+        intrinsic.AddParam("self");
+        intrinsic.code = delegate(TAC.Context context, Intrinsic.Result partialResult)
+        {
+            GreyMap greyMap = context.GetVar("self") as GreyMap;
+            if (greyMap != null)
+            {
+                GreyInterpreter greyInterpreter = (GreyInterpreter)context.interpreter;
+                var computer = greyInterpreter.hostData.GetComputer(greyMap).GetComputer();
+                if (computer != null)
+                {
+                    var router = computer.GetMainRouter();
+                    if (router != null)
+                    {
+                        var networkLan = router.GetMainRouter().GetNetworkLan();
+                        if (networkLan != null)
+                            networkLan.DisconnectComputer(computer);
+                        MessageClient messageClient = new MessageClient(IdClient.EnableEthernetClientRpc);
+                        messageClient.AddString(new List<string> { "Network Disconnected", "" });
+                        messageClient.AddBool(false);
+                        greyInterpreter.SendData(messageClient);
+                        return Intrinsic.Result.True;
+                    }
+                    return new Intrinsic.Result("Unable to get router");
+                }
+                return new Intrinsic.Result("Unable to get computer");
+            }
+            return Intrinsic.Result.Null;
+        };
+    }
+}
---
+++ patches/PlayerUtilsPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+[HarmonyPatch]
+public class PlayerUtilsPatch
+{
+    [HarmonyPatch(typeof(PlayerUtils), "ConfigLanguage")]
+    class ConfigLanguagePatch
+    {
+        private static Dictionary<string, string> contents = new Dictionary<string, string>()
+        {
+            { "DOC_COMPUTER_DISCONNECT_NETWORK", "<mark=#00D0124D><b>[Parameters]</b></mark>\n<color=#33cccc>None</color>\n\n<mark=#00D0124D><b>[Description]</b></mark>\nDisconnects the current device from any network it is connected to." }
+        };
+
+        static void Postfix()
+        {
+            foreach (var content in contents)
+            {
+                TranslationSystem.Singleton.rootTexts["English"].content.Add(content.Key, content.Value);
+            }
+        }
+    }
+}
